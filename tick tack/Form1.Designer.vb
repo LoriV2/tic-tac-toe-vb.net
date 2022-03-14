2a34245c48edb736ac79eee6d861f1c43d438482
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-03-14 16:12:24 +0000

    Dodaj pliki projektów.

---
+++ tick tack/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.X_O_1 = New System.Windows.Forms.Button()
+        Me.X_O_2 = New System.Windows.Forms.Button()
+        Me.X_O_3 = New System.Windows.Forms.Button()
+        Me.X_O_5 = New System.Windows.Forms.Button()
+        Me.X_O_6 = New System.Windows.Forms.Button()
+        Me.X_O_4 = New System.Windows.Forms.Button()
+        Me.X_O_7 = New System.Windows.Forms.Button()
+        Me.X_O_8 = New System.Windows.Forms.Button()
+        Me.X_O_9 = New System.Windows.Forms.Button()
+        Me.Player_turn_label = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Start_btn = New System.Windows.Forms.Button()
+        Me.O_wis = New System.Windows.Forms.Label()
+        Me.X_win = New System.Windows.Forms.Label()
+        Me.O_wins = New System.Windows.Forms.Label()
+        Me.X_wins = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'X_O_1
+        '
+        Me.X_O_1.FlatAppearance.BorderSize = 0
+        Me.X_O_1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_1.Location = New System.Drawing.Point(37, 30)
+        Me.X_O_1.Name = "X_O_1"
+        Me.X_O_1.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_1.TabIndex = 0
+        Me.X_O_1.UseVisualStyleBackColor = True
+        '
+        'X_O_2
+        '
+        Me.X_O_2.FlatAppearance.BorderSize = 0
+        Me.X_O_2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_2.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_2.Location = New System.Drawing.Point(116, 30)
+        Me.X_O_2.Name = "X_O_2"
+        Me.X_O_2.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_2.TabIndex = 1
+        Me.X_O_2.UseVisualStyleBackColor = True
+        '
+        'X_O_3
+        '
+        Me.X_O_3.FlatAppearance.BorderSize = 0
+        Me.X_O_3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_3.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_3.Location = New System.Drawing.Point(195, 30)
+        Me.X_O_3.Name = "X_O_3"
+        Me.X_O_3.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_3.TabIndex = 2
+        Me.X_O_3.UseVisualStyleBackColor = True
+        '
+        'X_O_5
+        '
+        Me.X_O_5.FlatAppearance.BorderSize = 0
+        Me.X_O_5.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_5.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_5.Location = New System.Drawing.Point(116, 98)
+        Me.X_O_5.Name = "X_O_5"
+        Me.X_O_5.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_5.TabIndex = 3
+        Me.X_O_5.UseVisualStyleBackColor = True
+        '
+        'X_O_6
+        '
+        Me.X_O_6.FlatAppearance.BorderSize = 0
+        Me.X_O_6.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_6.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_6.Location = New System.Drawing.Point(195, 98)
+        Me.X_O_6.Name = "X_O_6"
+        Me.X_O_6.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_6.TabIndex = 4
+        Me.X_O_6.UseVisualStyleBackColor = True
+        '
+        'X_O_4
+        '
+        Me.X_O_4.FlatAppearance.BorderSize = 0
+        Me.X_O_4.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_4.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_4.Location = New System.Drawing.Point(37, 98)
+        Me.X_O_4.Name = "X_O_4"
+        Me.X_O_4.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_4.TabIndex = 5
+        Me.X_O_4.UseVisualStyleBackColor = True
+        '
+        'X_O_7
+        '
+        Me.X_O_7.FlatAppearance.BorderSize = 0
+        Me.X_O_7.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_7.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_7.Location = New System.Drawing.Point(37, 166)
+        Me.X_O_7.Name = "X_O_7"
+        Me.X_O_7.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_7.TabIndex = 6
+        Me.X_O_7.UseVisualStyleBackColor = True
+        '
+        'X_O_8
+        '
+        Me.X_O_8.FlatAppearance.BorderSize = 0
+        Me.X_O_8.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_8.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_8.Location = New System.Drawing.Point(116, 166)
+        Me.X_O_8.Name = "X_O_8"
+        Me.X_O_8.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_8.TabIndex = 7
+        Me.X_O_8.UseVisualStyleBackColor = True
+        '
+        'X_O_9
+        '
+        Me.X_O_9.FlatAppearance.BorderSize = 0
+        Me.X_O_9.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red
+        Me.X_O_9.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.X_O_9.Location = New System.Drawing.Point(195, 166)
+        Me.X_O_9.Name = "X_O_9"
+        Me.X_O_9.Size = New System.Drawing.Size(73, 62)
+        Me.X_O_9.TabIndex = 8
+        Me.X_O_9.UseVisualStyleBackColor = True
+        '
+        'Player_turn_label
+        '
+        Me.Player_turn_label.AutoSize = True
+        Me.Player_turn_label.Location = New System.Drawing.Point(141, 9)
+        Me.Player_turn_label.Name = "Player_turn_label"
+        Me.Player_turn_label.Size = New System.Drawing.Size(16, 15)
+        Me.Player_turn_label.TabIndex = 9
+        Me.Player_turn_label.Text = "..."
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Start_btn
+        '
+        Me.Start_btn.Location = New System.Drawing.Point(307, 30)
+        Me.Start_btn.Name = "Start_btn"
+        Me.Start_btn.Size = New System.Drawing.Size(73, 23)
+        Me.Start_btn.TabIndex = 10
+        Me.Start_btn.Text = "Start"
+        Me.Start_btn.UseVisualStyleBackColor = True
+        '
+        'O_wis
+        '
+        Me.O_wis.AutoSize = True
+        Me.O_wis.Location = New System.Drawing.Point(274, 77)
+        Me.O_wis.Name = "O_wis"
+        Me.O_wis.Size = New System.Drawing.Size(46, 15)
+        Me.O_wis.TabIndex = 11
+        Me.O_wis.Text = "O wins:"
+        '
+        'X_win
+        '
+        Me.X_win.AutoSize = True
+        Me.X_win.Location = New System.Drawing.Point(274, 98)
+        Me.X_win.Name = "X_win"
+        Me.X_win.Size = New System.Drawing.Size(44, 15)
+        Me.X_win.TabIndex = 12
+        Me.X_win.Text = "X wins:"
+        '
+        'O_wins
+        '
+        Me.O_wins.AutoSize = True
+        Me.O_wins.Location = New System.Drawing.Point(324, 77)
+        Me.O_wins.Name = "O_wins"
+        Me.O_wins.Size = New System.Drawing.Size(13, 15)
+        Me.O_wins.TabIndex = 13
+        Me.O_wins.Text = "0"
+        '
+        'X_wins
+        '
+        Me.X_wins.AutoSize = True
+        Me.X_wins.Location = New System.Drawing.Point(324, 98)
+        Me.X_wins.Name = "X_wins"
+        Me.X_wins.Size = New System.Drawing.Size(13, 15)
+        Me.X_wins.TabIndex = 14
+        Me.X_wins.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(405, 252)
+        Me.Controls.Add(Me.X_wins)
+        Me.Controls.Add(Me.O_wins)
+        Me.Controls.Add(Me.X_win)
+        Me.Controls.Add(Me.O_wis)
+        Me.Controls.Add(Me.Start_btn)
+        Me.Controls.Add(Me.Player_turn_label)
+        Me.Controls.Add(Me.X_O_9)
+        Me.Controls.Add(Me.X_O_8)
+        Me.Controls.Add(Me.X_O_7)
+        Me.Controls.Add(Me.X_O_4)
+        Me.Controls.Add(Me.X_O_6)
+        Me.Controls.Add(Me.X_O_5)
+        Me.Controls.Add(Me.X_O_3)
+        Me.Controls.Add(Me.X_O_2)
+        Me.Controls.Add(Me.X_O_1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents X_O_1 As Button
+    Friend WithEvents X_O_2 As Button
+    Friend WithEvents X_O_3 As Button
+    Friend WithEvents X_O_5 As Button
+    Friend WithEvents X_O_6 As Button
+    Friend WithEvents X_O_4 As Button
+    Friend WithEvents X_O_7 As Button
+    Friend WithEvents X_O_8 As Button
+    Friend WithEvents X_O_9 As Button
+    Friend WithEvents Player_turn_label As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Start_btn As Button
+    Friend WithEvents O_wis As Label
+    Friend WithEvents X_win As Label
+    Friend WithEvents O_wins As Label
+    Friend WithEvents X_wins As Label
+End Class
---
+++ tick tack/Form1.vb
@@ -0,0 +1,265 @@
+﻿
+Public Class Form1
+    Public turns As Integer = 0
+    Public player1 As Boolean = True
+    Public who_wins As String
+    Public wins As Boolean = False
+    Public count_win_O As Integer = 0
+    Public count_win_X As Integer = 0
+
+    Public Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+    End Sub
+    Public Sub Winning()
+        'all posibilities to win for O
+        If (X_O_1.Text = "O" And X_O_2.Text = "O" And X_O_3.Text = "O") Or
+            (X_O_4.Text = "O" And X_O_5.Text = "O" And X_O_6.Text = "O") Or
+            (X_O_7.Text = "O" And X_O_8.Text = "O" And X_O_9.Text = "O") Or
+            (X_O_1.Text = "O" And X_O_4.Text = "O" And X_O_7.Text = "O") Or
+            (X_O_2.Text = "O" And X_O_5.Text = "O" And X_O_8.Text = "O") Or
+            (X_O_3.Text = "O" And X_O_6.Text = "O" And X_O_9.Text = "O") Or
+            (X_O_1.Text = "O" And X_O_5.Text = "O" And X_O_9.Text = "O") Or
+           (X_O_3.Text = "O" And X_O_5.Text = "O" And X_O_7.Text = "O") Then
+
+            count_win_O += 1
+            O_wins.Text = count_win_O
+            Timer1.Enabled = False
+            who_wins = "player 1 wins"
+            Player_turn_label.Text = who_wins
+            wins = True
+
+            'all posibilities to win for X
+        ElseIf (X_O_1.Text = "X" And X_O_2.Text = "X" And X_O_3.Text = "X") Or
+            (X_O_4.Text = "X" And X_O_5.Text = "X" And X_O_6.Text = "X") Or
+            (X_O_7.Text = "X" And X_O_8.Text = "X" And X_O_9.Text = "X") Or
+            (X_O_1.Text = "X" And X_O_4.Text = "X" And X_O_7.Text = "X") Or
+            (X_O_2.Text = "X" And X_O_5.Text = "X" And X_O_8.Text = "X") Or
+            (X_O_3.Text = "X" And X_O_6.Text = "X" And X_O_9.Text = "X") Or
+            (X_O_1.Text = "X" And X_O_5.Text = "X" And X_O_9.Text = "X") Or
+           (X_O_3.Text = "X" And X_O_5.Text = "X" And X_O_7.Text = "X") Then
+
+            count_win_X += 1
+            X_wins.Text = count_win_X
+            Timer1.Enabled = False
+            who_wins = "player 2 wins"
+            Player_turn_label.Text = who_wins
+            wins = True
+
+            'if draw
+        ElseIf turns = 8 Then
+            Timer1.Enabled = False
+            who_wins = "draw"
+            Player_turn_label.Text = who_wins
+            wins = True
+
+        Else
+            who_wins = "...."
+
+        End If
+
+    End Sub
+
+    Public Sub X_O_5_Click(sender As Object, e As EventArgs) Handles X_O_5.Click
+        'if it is O turn
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_5.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_5.Enabled = False
+            Winning()
+            'if it is X turn
+        ElseIf Timer1.Enabled = True Then
+            X_O_5.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_5.Enabled = False
+            Winning()
+        End If
+        turns += 1
+
+    End Sub
+
+    Public Sub X_O_1_Click(sender As Object, e As EventArgs) Handles X_O_1.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_1.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_1.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_1.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_1.Enabled = False
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub X_O_2_Click(sender As Object, e As EventArgs) Handles X_O_2.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_2.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_2.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_2.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_2.Enabled = False
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub X_O_3_Click(sender As Object, e As EventArgs) Handles X_O_3.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_3.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_3.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_3.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_3.Enabled = False
+            Winning()
+        End If
+        turns += 1
+
+    End Sub
+
+    Public Sub X_O_4_Click(sender As Object, e As EventArgs) Handles X_O_4.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_4.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_4.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_4.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_4.Enabled = False
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub X_O_6_Click(sender As Object, e As EventArgs) Handles X_O_6.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_6.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_6.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_6.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_6.Enabled = False
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub X_O_7_Click(sender As Object, e As EventArgs) Handles X_O_7.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_7.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_7.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_7.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            X_O_7.Enabled = False
+            player1 = True
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub X_O_8_Click(sender As Object, e As EventArgs) Handles X_O_8.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_8.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_8.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_8.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_8.Enabled = False
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub X_O_9_Click(sender As Object, e As EventArgs) Handles X_O_9.Click
+        If player1 = True And Timer1.Enabled = True Then
+            X_O_9.Text = "O"
+            Player_turn_label.Text = "Player 2 turn"
+            player1 = False
+            X_O_9.Enabled = False
+            Winning()
+        ElseIf Timer1.Enabled = True Then
+            X_O_9.Text = "X"
+            Player_turn_label.Text = "Player 1 turn"
+            player1 = True
+            X_O_9.Enabled = False
+            Winning()
+        End If
+
+        turns += 1
+    End Sub
+
+    Public Sub Start_btn_Click(sender As Object, e As EventArgs) Handles Start_btn.Click
+        'if there was no win or draw yet
+        If wins = False Then
+            Player_turn_label.Text = "Player 1 turn"
+            Timer1.Enabled = True
+            'if you want to restart after played game
+        ElseIf wins = True And (who_wins = "player 1 wins" Or who_wins = "player 2 wins" Or who_wins = "draw") Then
+
+            X_O_1.Text = ""
+            X_O_2.Text = ""
+            X_O_3.Text = ""
+            X_O_4.Text = ""
+            X_O_5.Text = ""
+            X_O_6.Text = ""
+            X_O_7.Text = ""
+            X_O_8.Text = ""
+            X_O_9.Text = ""
+            X_O_1.Enabled = True
+            X_O_2.Enabled = True
+            X_O_3.Enabled = True
+            X_O_4.Enabled = True
+            X_O_5.Enabled = True
+            X_O_6.Enabled = True
+            X_O_7.Enabled = True
+            X_O_8.Enabled = True
+            X_O_9.Enabled = True
+            player1 = True
+            Player_turn_label.Text = "Player 1 turn"
+            who_wins = "....."
+            turns = 0
+            Timer1.Enabled = True
+
+        End If
+    End Sub
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+End Class
